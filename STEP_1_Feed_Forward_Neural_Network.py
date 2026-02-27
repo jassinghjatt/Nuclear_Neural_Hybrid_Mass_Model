ed0945feec0686e3ad8869a4a68167726c1ffcbe
Jaskirat Singh <153146576+jassinghjatt@users.noreply.github.com>
2026-02-27 13:52:07 +0530

    Update and rename bin_all.py to STEP_1_Feed_Forward_Neural_Network.py

--- STEP_1_Feed_Forward_Neural_Network.py
+++ STEP_1_Feed_Forward_Neural_Network.py
@@ -1,8 +1,5 @@
-# ============================================================
-# RESIDUAL BOOSTING FFNN (NO BINS, WITH MASKING)
-# SINGLE-GROUP SCRIPT VERSION
-# ============================================================
-
+'''This code takes in the file 'nuclei_all.xlsx' which is reuiqred to have at least the Z, N, A, AME, M_th, deltaM= AME- M_th for training.
+All other features(like S_n etc.) can be removed or new features can be added if required.'''
 import sys
 import numpy as np
 import pandas as pd
@@ -14,15 +11,13 @@ from math import sqrt
 import gc
 import os
 print("Process ID:", os.getpid())
-# ============================================================
-# READ GROUP NUMBER FROM TERMINAL
-# ============================================================
+
+''' This is a .py file which can be called by the STEP_1_FFNN.ipynb file. The STEP_1_FFNN.ipynb file will use this code to loop over 22 random groups of nuclei. 
+We train over other 21 groups of nuclei and test over 22nd group and repeat this for all the groups and then compile the test data.'''
 
 TEST_GROUP = int(sys.argv[1])
 
-# ============================================================
 # SETTINGS
-# ============================================================
 
 SEED = 42
 np.random.seed(SEED)
@@ -33,8 +28,11 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
-PLATEAU_EPOCHS = 10
-MAX_EPOCHS = 500
+PLATEAU_EPOCHS = 10 
+'''we are using this because out of hundreds of those neural networks, 90% are reduandant for a given group(but they are random for different groups)
+So, when the change in the loss is less than the desired amount, we simply stop the current NN(neural network) and move on to the next one with decreased regularisation.'''
+
+MAX_EPOCHS = 500 #To prevent overfitting.
 
 os.makedirs("results_residual_ffnn_singlebin", exist_ok=True)
 
@@ -42,11 +40,9 @@ print("\n" + "="*70)
 print(f"MAJOR RUN — TEST GROUP {TEST_GROUP}")
 print("="*70)
 
-# ============================================================
 # LOAD DATA
-# ============================================================
 
-df = pd.read_excel("A<121_nuclei_all.xlsx")
+df = pd.read_excel("nuclei_all.xlsx")
 groups_df = pd.read_excel("splits/random_22_groups.xlsx")
 df = df.merge(groups_df, on=["Z","N"], how="left")
 
@@ -59,7 +55,7 @@ feature_cols = [
     "dZ_magic","dN_magic","dZ2_magic","dN2_magic",
     "N-Z",
     "deltaM_local_mean_bous","deltaM_local_var_bous"
-]
+]#these are all the features that we used for training, we may remove them if not needed.
 
 train_df = df[df["group"] != TEST_GROUP].copy()
 test_df  = df[df["group"] == TEST_GROUP].copy()
@@ -71,9 +67,7 @@ X_test  = scaler.transform(test_df[feature_cols].values)
 delta_train = train_df["deltaM"].values.copy()
 delta_test  = test_df["deltaM"].values.copy()
 
-# ============================================================
 # BUILD FFNN
-# ============================================================
 
 def build_ffnn(input_dim, l2_value):
 
@@ -101,10 +95,8 @@ def build_ffnn(input_dim, l2_value):
 
     return model
 
-# ============================================================
 # L2 SCHEDULE
-# ============================================================
-
+'''We begin from large regularisation and slowly decrease it to squeeze out as much as global trend information possible.'''
 L2_schedule = []
 L2_schedule += list(np.arange(10000e-5, 3000e-5 - 1e-12, -100e-5))
 L2_schedule += list(np.arange(2950e-5, 500e-5 - 1e-12, -50e-5))
@@ -114,20 +106,20 @@ L2_schedule += list(np.arange(37e-5, 1e-5 - 1e-12, -0.5e-5))
 L2_schedule += list(np.arange(9e-6, 1e-6 - 1e-12, -4e-6))
 L2_schedule += list(np.arange(9e-7, 1e-7 - 1e-12, -4e-7))
 
-# ============================================================
 # TRAINING LOOP
-# ============================================================
 
 for step_index, current_l2 in enumerate(L2_schedule):
 
     print(f"\nL2 STEP {step_index+1} | L2 = {current_l2:.8e}")
 
-    loss_threshold = 0.02 if current_l2 >= 2e-4 else 0.009
+    loss_threshold = 0.02 if current_l2 >= 2e-4 else 0.009 
+    '''because after some point the loss function usually decreases very less so to avoid skipping necessary steps, we decreased the loss threshold'''
 
     extreme_weight = 0.2 if current_l2 >= 2e-2 else 0.1
+    '''Some nuclei are largely out of bounds and interfere with the learning of other nuclei, so we mask them.'''
     sample_weights = np.ones(len(delta_train))
 
-    mask = (delta_train < -2) | (delta_train > 2)
+    mask = (delta_train < -2) | (delta_train > 2) #2 is in MeV.
     sample_weights[mask] = extreme_weight
 
     model = build_ffnn(X_train.shape[1], current_l2)
@@ -186,7 +178,7 @@ for step_index, current_l2 in enumerate(L2_schedule):
     tf.keras.backend.clear_session()
     gc.collect()
 
-    if train_rmse <= 0.025:
+    if train_rmse <= 0.025:  # it is mostly noise beyond this point so we stop here.
         print("*** TARGET TRAIN RMSE REACHED — STOPPING GROUP ***")
         break
 
@@ -195,8 +187,8 @@ for step_index, current_l2 in enumerate(L2_schedule):
 # ============================================================
 
 output_df = test_df[["Z","N","A"]].copy()
-output_df["deltaM"] = test_df["deltaM"]
-output_df["deltaMi_final"] = delta_test
+output_df["deltaM"] = test_df["deltaM"] #deltaM is the original deltaM
+output_df["deltaMi_final"] = delta_test   #deltaMi_final is the one learnt and predicted by the NN.
 
 save_path = f"results_residual_ffnn_singlebin/Test_Group_{TEST_GROUP}_Final.xlsx"
 output_df.to_excel(save_path, index=False)
